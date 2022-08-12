ebd724a2e9d9447c2ea69bb97c8f1d4d1e4202e3
kmanley <kyle.manley@contracts365.com>
2022-08-12 16:31:01 -0400

    Refactored and unit tested a return of status 200 if valid zipcode

---
+++ TestHelpers.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YourWeatherInfo_Functions.Tests
+{
+    public class TestHelpers
+    {
+        public static HttpRequest HttpRequestSetup(Dictionary<String, StringValues> query, string body, IHeaderDictionary headers = null)
+        {
+            var reqMock = new Mock<HttpRequest>();
+            var httpContextMock = new Mock<HttpContext>();
+            var connectionInfoMock = new Mock<ConnectionInfo>();
+
+            reqMock.Setup(req => req.Query).Returns(new QueryCollection(query));
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(body);
+            writer.Flush();
+            stream.Position = 0;
+            reqMock.Setup(req => req.Body).Returns(stream);
+
+            connectionInfoMock.Setup(x => x.RemoteIpAddress).Returns(new System.Net.IPAddress(3232235777));
+
+            httpContextMock.Setup(x => x.Connection).Returns(connectionInfoMock.Object);
+
+            reqMock.Setup(req => req.HttpContext).Returns(httpContextMock.Object);
+
+            reqMock.Setup(req => req.Headers).Returns(headers);
+            return reqMock.Object;
+        }
+    }
+}
--- WeatherRequestTests.cs
+++ WeatherRequestTests.cs
@@ -1,4 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace YourWeatherInfo_Functions.Tests
@@ -6,16 +12,35 @@ namespace YourWeatherInfo_Functions.Tests
     public class WeatherRequestTests
     {
         [Fact]
-        public void Test1()
+        public async Task Return_200_If_Valid_ZipCode()
         {
-            string zipcode = "01950";
-            Assert.Equal("01950", zipcode);
-        }
-        [Fact]
-        public void Test2()
-        {
-            string zipcode = "01950";
-            Assert.Equal("01915", zipcode);
+            // Given
+            Mock<IWeatherService> mockWeatherService = new Mock<IWeatherService>();
+            mockWeatherService
+                .Setup(x => x.GetWeatherRecord(It.IsAny<ILogger>(), "01970"))
+                .ReturnsAsync(new WeatherRecord
+                {
+                    WeatherRecordJson = "testtestest"
+                });
+
+            WeatherRequest weatherRequest = new WeatherRequest(mockWeatherService.Object);
+
+            JObject request = new JObject
+            {
+                ["zipcode"] = "01970"
+            };
+
+            var httpRequest = TestHelpers.HttpRequestSetup(new System.Collections.Generic.Dictionary<string, Microsoft.Extensions.Primitives.StringValues>(), request.ToString());
+
+            // When
+
+            OkObjectResult result = (OkObjectResult)await weatherRequest.Run(httpRequest, NullLogger.Instance);
+            var expected = result.Value;
+
+            // Then
+
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("testtestest", expected);
         }
     }
 }
